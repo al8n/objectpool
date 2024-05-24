5df544afa8ab7acc1f732e0f29eba554d07bf0cf
Al Liu <scygliu1@gmail.com>
2024-05-24 23:39:58 +0800

    cleanup

--- ci/sanitizer.sh
+++ ci/sanitizer.sh
@@ -6,12 +6,12 @@ export ASAN_OPTIONS="detect_odr_violation=0 detect_leaks=0"
 
 # Run address sanitizer with cargo-hack
 RUSTFLAGS="-Z sanitizer=address" \
-cargo test
+cargo test --tests
 
 # Run leak sanitizer with cargo-hack
 RUSTFLAGS="-Z sanitizer=leak" \
-cargo test
+cargo test --tests
 
 # Run thread sanitizer with cargo-hack
 RUSTFLAGS="-Z sanitizer=thread" \
-cargo -Zbuild-std test
+cargo -Zbuild-std test --tests
